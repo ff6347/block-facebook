eb1ffab2034ef49006672d140391dfc365964a72
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2018-12-23 21:21:16 +0100

    not on a branch to push

--- .travis/commit-and-push.sh
+++ .travis/commit-and-push.sh
@@ -3,6 +3,9 @@ TODAY=$(date)
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
+git stash
+git checkout master
+git stash pop
 git add block-facebook.lsrules
 git commit "auto-update with travis ${TODAY}"
 git push https://${TRAVIS_DEPLOY_TOKEN}@github.com/fabianmoronzirfas/block-facebook.git
