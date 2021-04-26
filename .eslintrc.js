d3e91386f937f8308394afce4cbbdc1d3dfd4677
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-04-26 10:05:27 +0200

    update things

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = require("@inpyjamas/scripts/eslint")
--- build-scripts/index.ts
+++ build-scripts/index.ts
@@ -1,41 +1,53 @@
-import {promises} from 'fs';
-import {resolve} from 'path';
+import { promises } from "fs";
+import { resolve } from "path";
 
 const readFile = promises.readFile;
 const writeFile = promises.writeFile;
 // tslint:disable-next-line:no-console
 // console.log('hello .ts');
-
-(async () => {
+async function main() {
   let match;
   const results = [];
   const regex = /\d.\d.\d.\d\s(.*?)\n/g;
 
   const blocklist = await readFile(
-      resolve(__dirname, '../blocklists/corporations/facebook/all'), 'utf8').catch(error => {
-        throw error;
-      });
+    resolve(__dirname, "../blocklists/corporations/facebook/all"),
+    "utf8"
+  ).catch((error) => {
+    throw error;
+  });
 
   // tslint:disable-next-line:no-conditional-assignment
-  while (match = regex.exec(blocklist)) {
+  while ((match = regex.exec(blocklist))) {
     results.push(match[1]);
   }
 
-  const lsrules = await readFile(resolve(__dirname, '../block-facebook.lsrules'), 'utf8').catch(error => {
+  const lsrules = await readFile(
+    resolve(__dirname, "../block-facebook.lsrules"),
+    "utf8"
+  ).catch((error) => {
     throw error;
   });
 
   const parsedLSrules = JSON.parse(lsrules);
-  const alldomains = parsedLSrules.rules[0]['remote-domains'];
+  const alldomains = parsedLSrules.rules[0]["remote-domains"];
   let newDomains = [...new Set([...alldomains, ...results])];
   newDomains = newDomains.sort((a, b) => {
-    if (a < b) { return -1; } else if (a > b) { return 1; }
+    if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return 1;
+    }
     return 0;
   });
-  parsedLSrules.rules[0]['remote-domains'] = newDomains;
+  parsedLSrules.rules[0]["remote-domains"] = newDomains;
 
-  await writeFile(resolve(__dirname, '../block-facebook.lsrules'),
-  JSON.stringify(parsedLSrules, null, 2)).catch(error => {
+  await writeFile(
+    resolve(__dirname, "../block-facebook.lsrules"),
+    JSON.stringify(parsedLSrules, null, 2)
+  ).catch((error) => {
     throw error;
   });
-})();
+}
+
+main().catch(console.error);
